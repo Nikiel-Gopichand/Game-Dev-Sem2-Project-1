bb89400f48c3dffb86dc822b15b81bd717b27a20
Nikiel-Gopichand <1844475@students.wits.ac.za>
2021-08-13 19:15:31 +0200

    Slots Working, Grid Set up

--- Prototype Project/Assets/scripts/Slots.cs
+++ Prototype Project/Assets/scripts/Slots.cs
@@ -1,15 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slots : MonoBehaviour
 {
+    public Image result1;
+    public Image result2;
+    public Image result3;
+
+    public Sprite gem1;
+    public Sprite gem2;
+    public Sprite gem3;
+    Image[] slotArr = new Image[3];
     // Start is called before the first frame update
     void Start()
     {
         
-    }
+        slotArr[0] = result1;
+        slotArr[1] = result2;
+        slotArr[2] = result3;
 
+    }
+    public void onRoll()
+    {
+        float rand;
+
+
+        for (int i = 0; i < 3; i++)
+        {
+            rand = Random.Range(0, 100);
+            slotArr[i].sprite = (rand < 33 ? gem1 : rand > 33 && rand < 66 ? gem2 : gem3);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
---
+++ Prototype Project/Assets/scripts/grid.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grid : MonoBehaviour
+{
+    public GameObject up;
+    public GameObject down;
+    public GameObject left;
+    public GameObject right;
+    bool open;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    public bool isOpen()
+    {
+        return open;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
